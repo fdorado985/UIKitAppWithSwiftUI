7dc67bc66577a8b27080c7f9cd38ea8eb68c0269
Juan Dorado <fdorado985@gmail.com>
2020-11-07 12:41:02 -0600

    Connect the components to controller and create the logic to pass the data

--- UIKitAppWithSwiftUI/ViewController.swift
+++ UIKitAppWithSwiftUI/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import SwiftUI
 
 class ViewController: UIViewController {
+  @IBOutlet weak var dataTextField: UITextField!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -17,6 +18,14 @@ class ViewController: UIViewController {
   @IBAction func openSwiftUIViewWithCodeDidTap(_ sender: UIButton) {
     let profileView = UIHostingController(rootView: ProfileView())
     navigationController?.pushViewController(profileView, animated: true)
+  }
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "toShowDataView" {
+      if let hostingController = segue.destination as? ShowDataViewHostingViewController {
+        hostingController.rootView = ShowDataView(dataTextField: dataTextField)
+      }
+    }
   }
 }
 
