94824ce5a987ffe3d383a1f560e06d1685a2785b
Juan Dorado <fdorado985@gmail.com>
2020-11-07 02:08:12 -0600

    Create a `SettingsView` with SwiftUI

---
+++ UIKitAppWithSwiftUI/SwiftUI Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  UIKitAppWithSwiftUI
+//
+//  Created by Juan Francisco Dorado Torres on 07/11/20.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  var body: some View {
+    Text("Settings")
+  }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+  static var previews: some View {
+    SettingsView()
+  }
+}
