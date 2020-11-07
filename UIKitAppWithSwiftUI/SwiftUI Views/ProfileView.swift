8cb895f04b1f693397eaf65f2f79f6aaa62c17c4
Juan Dorado <fdorado985@gmail.com>
2020-11-07 02:22:20 -0600

    Create a `ProfileView` with a `Font` modifier on the Text to be `largeTitle`

---
+++ UIKitAppWithSwiftUI/SwiftUI Views/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  UIKitAppWithSwiftUI
+//
+//  Created by Juan Francisco Dorado Torres on 07/11/20.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+  var body: some View {
+    Text("Profile View")
+      .font(.largeTitle)
+  }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+  static var previews: some View {
+    ProfileView()
+  }
+}
