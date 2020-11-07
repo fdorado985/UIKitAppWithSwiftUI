a674fe0ec076973b8b479c227cd3c5b1fac14341
Juan Dorado <fdorado985@gmail.com>
2020-11-07 12:27:57 -0600

    Create another SwiftUI View to pass data

---
+++ UIKitAppWithSwiftUI/SwiftUI Views/ShowDataView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowDataView.swift
+//  UIKitAppWithSwiftUI
+//
+//  Created by Juan Francisco Dorado Torres on 07/11/20.
+//
+
+import SwiftUI
+
+struct ShowDataView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct ShowDataView_Previews: PreviewProvider {
+  static var previews: some View {
+    ShowDataView()
+  }
+}
