584ced15cc093a9f90caad4945a4d5f4e5888d64
Juan Dorado <fdorado985@gmail.com>
2020-11-08 12:34:06 -0600

    Create the SwiftUI View to send data back to the UIKit controller

---
+++ UIKitAppWithSwiftUI/SwiftUI Views/SendDataBackToUIKit.swift
@@ -0,0 +1,35 @@
+//
+//  SendDataBackToUIKit.swift
+//  UIKitAppWithSwiftUI
+//
+//  Created by Juan Francisco Dorado Torres on 08/11/20.
+//
+
+import SwiftUI
+
+struct SendDataBackToUIKit: View {
+  var sendNameBack: ((String) -> Void)?
+  @State var name: String = ""
+
+  var body: some View {
+    VStack {
+      Text("Enter name:")
+      TextField("Name", text: $name)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .padding()
+      Button(action: {
+        guard let sendNameBack = sendNameBack else { return }
+        sendNameBack(name)
+      }) {
+        Text("Update name")
+      }
+    }
+    .padding(16.0)
+  }
+}
+
+struct SendDataBackToUIKit_Previews: PreviewProvider {
+  static var previews: some View {
+    SendDataBackToUIKit()
+  }
+}
