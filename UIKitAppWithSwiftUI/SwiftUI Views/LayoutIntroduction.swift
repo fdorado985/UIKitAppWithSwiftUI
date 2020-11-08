ce1f955b0913ceea421812dc147c746af180341c
Juan Dorado <fdorado985@gmail.com>
2020-11-08 16:31:22 -0600

    Create the View for Layout Introductio

---
+++ UIKitAppWithSwiftUI/SwiftUI Views/LayoutIntroduction.swift
@@ -0,0 +1,20 @@
+//
+//  LayoutIntroduction.swift
+//  UIKitAppWithSwiftUI
+//
+//  Created by Juan Francisco Dorado Torres on 08/11/20.
+//
+
+import SwiftUI
+
+struct LayoutIntroduction: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct LayoutIntroduction_Previews: PreviewProvider {
+  static var previews: some View {
+    LayoutIntroduction()
+  }
+}
