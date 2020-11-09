5bccb6e4207ca735d676288e6d82c33898b87620
Juan Dorado <fdorado985@gmail.com>
2020-11-08 18:17:25 -0600

    Create the file to build a complex UI

---
+++ UIKitAppWithSwiftUI/SwiftUI Views/ComplexUIView.swift
@@ -0,0 +1,20 @@
+//
+//  ComplexUIView.swift
+//  UIKitAppWithSwiftUI
+//
+//  Created by Juan Francisco Dorado Torres on 08/11/20.
+//
+
+import SwiftUI
+
+struct ComplexUIView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct ComplexUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    ComplexUIView()
+  }
+}
