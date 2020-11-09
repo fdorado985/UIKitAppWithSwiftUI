58d62ac5ceba3e9e98cf7ca8f6df6572909a2ba5
Juan Dorado <fdorado985@gmail.com>
2020-11-08 18:19:07 -0600

    Add the Background color

--- UIKitAppWithSwiftUI/SwiftUI Views/ComplexUIView.swift
+++ UIKitAppWithSwiftUI/SwiftUI Views/ComplexUIView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ComplexUIView: View {
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack {
+      // Back layer
+      Color(#colorLiteral(red: 0.9788002372, green: 0.9369693398, blue: 0.8757455945, alpha: 1))
+        .ignoresSafeArea()
+
+      // Layer on Top
+      VStack {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      }
+    }
   }
 }
 
