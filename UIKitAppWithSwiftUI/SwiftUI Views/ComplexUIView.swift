d2f01211fcacebd58bea4ad3fac282ff9990dc14
Juan Dorado <fdorado985@gmail.com>
2020-11-09 00:12:30 -0600

    Create the card at the bottom

--- UIKitAppWithSwiftUI/SwiftUI Views/ComplexUIView.swift
+++ UIKitAppWithSwiftUI/SwiftUI Views/ComplexUIView.swift
@@ -70,15 +70,35 @@ struct ComplexUIView: View {
           Spacer()
           Text("25 tasks")
         }
+
         VStack(alignment: .leading, spacing: 40.0) {
-          Text("Recently Assigned")
-            .font(.title)
-            .fontWeight(.bold)
-          Text("Mobile App")
-            .fontWeight(.bold)
-          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales enim diam, nec lobortis tortor finibus at.")
-          Text("12 Oct 2020")
+          HStack {
+            Text("Recently Assigned")
+              .font(.title2)
+              .fontWeight(.bold)
+            Spacer()
+            Text("All Tasks")
+              .font(.footnote)
+          }
+          VStack(alignment: .leading, spacing: 20.0) {
+            Text("Mobile App")
+              .fontWeight(.bold)
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales enim diam, nec lobortis tortor finibus at.")
+            Text("12 Oct 2020")
+          }
+          .padding()
+          .background(
+            RoundedRectangle(cornerRadius: 25.0)
+              .fill(Color(#colorLiteral(red: 0.9646013379, green: 0.9647662044, blue: 0.9645908475, alpha: 1)))
+              .shadow(color: /*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/, radius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+          )
         }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(
+          RoundedRectangle(cornerRadius: 25.0)
+            .fill(Color(#colorLiteral(red: 0.9646013379, green: 0.9647662044, blue: 0.9645908475, alpha: 1)))
+        )
       }
       .padding()
     }
